c2c84f620112a91136933c7ac55e711159d94c09
maithilysawant <maithily1803@gmail.com>
2026-04-18 16:08:13 +0530

    db

--- app/config.php
+++ app/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'kayaX_db');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'sql211.infinityfree.com');
+define('DB_NAME', 'if0_41692918_kayaX');
+define('DB_USER', 'if0_41692918');
+define('DB_PASS', '18032003Ms');
 define('DB_CHARSET', 'utf8mb4');
 
 define('APP_NAME', 'kayaX');
